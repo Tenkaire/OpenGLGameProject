05239f7596be047229f80fc40f5c42d1366b4c64
Tenkaire <tenkaire958825@gmail.com>
2018-03-12 14:47:24 -0700

    Adding RawModel Class, Renderer Class, and Loader Class.
    This is learning VAOs and VBOs.

--- src/engineTester/MainGame.java
+++ src/engineTester/MainGame.java
@@ -3,17 +3,40 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGame {
 
 	public static void main(String[] args) {
 		DisplayManager.createDisplay();
 		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		float[] vertices = {
+				-0.5f, 0.5f, 0f,
+				-0.5f, -0.5f, 0f,
+				0.5f, -0.5f,0f, // left bottom triangle
+				
+				0.5f, -0.5f, 0f,
+				0.5f, 0.5f, 0f,
+				-0.5f, 0.5f, 0f // right top triangle
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
+		
 		while(!Display.isCloseRequested()) {
+			
+			renderer.prepare();
 			//game logic
 			// rendering
+			renderer.render(model);
 			DisplayManager.updateDisplay();
 		}
+		
+		loader.DeleteAll();
 		
 		DisplayManager.updateDisplay();
 
