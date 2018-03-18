268b8df9342f3c561ab303206bb4b3a04ef10d45
Tenkaire <tenkaire958825@gmail.com>
2018-03-17 19:27:41 -0700

    + Matrices and Uniform Variables

--- src/shaders/staticShader.java
+++ src/shaders/staticShader.java
@@ -1,9 +1,13 @@
 package shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class staticShader extends shaderProgram{
 	
 	private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
 	private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
+	
+	private int location_transformationMatrix;
 
 	public staticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -15,6 +19,15 @@ public class staticShader extends shaderProgram{
 		super.bindAttribute(0, "position");
 		super.bindAttribute(1, "textureCoords");
 		
+	}
+
+	@Override
+	protected void getAllUniformLocations() {
+		location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+	}
+	
+	public void loadTransformationMatrix(Matrix4f matrix) {
+		super.loadMatrix(location_transformationMatrix, matrix);
 	}
 
 }
