813b4ef8bfa22d86cd9a0b2e51b3bedf8d33435e
Tenkaire <tenkaire958825@gmail.com>
2018-03-21 00:17:13 -0700

    + Loading 3D Models

--- src/engineTester/MainGame.java
+++ src/engineTester/MainGame.java
@@ -9,6 +9,7 @@ import models.RawModel;
 import models.TextureModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.staticShader;
 import textures.ModelTexture;
@@ -22,93 +23,14 @@ public class MainGame {
 		staticShader shader = new staticShader();
 		Renderer renderer = new Renderer(shader);
 		
-		float[] vertices = {            
-                -0.5f,0.5f,0,   
-                -0.5f,-0.5f,0,  
-                0.5f,-0.5f,0,   
-                0.5f,0.5f,0,        
-                 
-                -0.5f,0.5f,1,   
-                -0.5f,-0.5f,1,  
-                0.5f,-0.5f,1,   
-                0.5f,0.5f,1,
-                 
-                0.5f,0.5f,0,    
-                0.5f,-0.5f,0,   
-                0.5f,-0.5f,1,   
-                0.5f,0.5f,1,
-                 
-                -0.5f,0.5f,0,   
-                -0.5f,-0.5f,0,  
-                -0.5f,-0.5f,1,  
-                -0.5f,0.5f,1,
-                 
-                -0.5f,0.5f,1,
-                -0.5f,0.5f,0,
-                0.5f,0.5f,0,
-                0.5f,0.5f,1,
-                 
-                -0.5f,-0.5f,1,
-                -0.5f,-0.5f,0,
-                0.5f,-0.5f,0,
-                0.5f,-0.5f,1
-                 
-        };
-         
-        float[] textureCoords = {
-                 
-                0,0,
-                0,1,
-                1,1,
-                1,0,            
-                0,0,
-                0,1,
-                1,1,
-                1,0,            
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0
- 
-                 
-        };
-         
-        int[] indices = {
-                0,1,3,  
-                3,1,2,  
-                4,5,7,
-                7,5,6,
-                8,9,11,
-                11,9,10,
-                12,13,15,
-                15,13,14,   
-                16,17,19,
-                19,17,18,
-                20,21,23,
-                23,21,22
- 
-        };
-		
-		RawModel model = loader.loadToVAO(vertices,textureCoords, indices);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("boy"));
+		RawModel model = OBJLoader.loadOBJModel("stall", loader);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
 		TextureModel staticModel = new TextureModel(model, texture);
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-5),0,0,0,1);
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-20),0,0,0,1);
 		Camera camera = new Camera();
 		
 		while(!Display.isCloseRequested()) {
-			entity.increaseRotation(1, 1, 0);
+			entity.increaseRotation(0, 1, 0);
 			camera.move();
 			renderer.prepare();
 			//game logic
